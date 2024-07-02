7ca7b40e117e752605ffc9b4c1d2dffec9d126cf
Vi <for29you@gmail.com>
2024-07-02 19:27:43 +0300

    Finish

--- cats/test.py
+++ cats/test.py
@@ -1,3 +1,4 @@
+import json
 import os
 import tempfile
 import unittest
@@ -6,12 +7,12 @@ from cats import app, db
 from cats.models import Cat
 
 
-class FlaskrTestCase(unittest.TestCase):
+class CatsTestCase(unittest.TestCase):
+    """Тестирование котов."""
 
     def setUp(self):
         self.db_uri = 'sqlite:///' + os.path.join('../.testdb.db')
         app.config['TESTING'] = True
-        # app.config['WTF_CSRF_ENABLED'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = self.db_uri
         self.app = app.test_client()
         with app.app_context():
@@ -32,6 +33,11 @@ class FlaskrTestCase(unittest.TestCase):
             db.session.add(cat)
             db.session.commit()
 
+    def _get_cat(self, **kwargs):
+        with app.app_context():
+            cat = Cat.query.filter_by(**kwargs).first()
+            return cat
+
     def test_index(self):
         """Проверка шаблона."""
         self._add_cat(self.cat_name)
@@ -50,3 +56,24 @@ class FlaskrTestCase(unittest.TestCase):
         data = response.json
         self.assertIn(self.cat_name, data[0].values())
 
+    def test_cat_detail(self):
+        self._add_cat(self.cat_name)
+        cat = self._get_cat(name=self.cat_name)
+
+        response = self.app.get(f'/cats/{cat.id}/')
+
+        self.assertEqual(response.status_code, 200)
+        data = response.json
+        self.assertEqual(data.get('id'), cat.id)
+
+    def test_add_cat(self):
+        data = {'name': self.cat_name}
+
+        response = self.app.post(
+            f'/cats/',
+            data=json.dumps(data),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        data = response.json
+        self.assertEqual(data.get('name'), self.cat_name)
--- cats/views.py
+++ cats/views.py
@@ -1,16 +1,33 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 
-from cats import app
+from cats import app, db
 from cats.models import Cat
 
 
 @app.route('/')
-def index():  # put application's code here
+def index():
     cats = Cat.query.all()
     return render_template('index.html', cats=cats)
 
 
 @app.route('/cats/')
-def cats():  # put application's code here
+def cats():
     cats = Cat.query.all()
     return jsonify([{cat.id: cat.name} for cat in cats])
+
+
+@app.route('/cats/<int:id>/')
+# Параметром указывается имя переменной:
+def cat_detail(id):
+    cat = Cat.query.first_or_404(id)
+    return jsonify({'id': cat.id, 'name': cat.name})
+
+
+@app.route('/cats/', methods=['POST'])
+def create_cat():
+    data = request.json
+    cat = Cat(**data)
+    db.session.add(cat)
+    db.session.commit()
+
+    return jsonify({'id': cat.id, 'name': cat.name}), 201
